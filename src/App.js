d278f75e794a7c35b48efd78f82055036422f7e7
Marley <manfrededun@gmail.com>
2021-07-10 10:37:43 +0000

    Firebase Authentication Configured

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import NoteForm from './Components/NoteForm';
-import NoteList from './Components/NoteList';
-import NoteItem from './Components/NoteItem';
-import { useState} from 'react';
-import Editform from './Components/Editform';
 import React from 'react';
+import Dashboard from './pages/dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+
 
 class App extends React.Component{
 
   render(){
     return (
-      <div className="row">
-        <div className = "col-md-6 userinput">
-
-          <NoteForm addUser = {this.addUser} /> 
-        </div>
-        
-        <div className = "col-md-6 Notelist">
-          <NoteList />   
-        </div>
-        
-      </div>
-  
+      <BrowserRouter>
+      <Switch>
+        <Route exact path ="/" component={Dashboard}></Route>
+        <Route path ="/login" component={Login}></Route>
+        <Route path = "/signup" component={Signup}></Route>
+      </Switch>
+      </BrowserRouter>
     );
   }
 
---
+++ src/pages/dashboard.js
@@ -0,0 +1,29 @@
+import NoteForm from '../Components/NoteForm';
+import NoteList from '../Components/NoteList';
+import NoteItem from '../Components/NoteItem';
+import { useState} from 'react';
+import Editform from '../Components/Editform';
+import React from 'react';
+
+class Dashboard extends React.Component{
+
+  render(){
+    return (
+      <div className="row">
+        <div className = "col-md-6 userinput">
+
+          <NoteForm addUser = {this.addUser} /> 
+        </div>
+        
+        <div className = "col-md-6 Notelist">
+          <NoteList />   
+        </div>
+        
+      </div>
+  
+    );
+  }
+
+}
+export default Dashboard;
+
